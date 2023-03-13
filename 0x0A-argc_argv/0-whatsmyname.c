e360c54436e2036fc285cedab60702e4b53afa3a
kutch57 <tyndaleobilor@gmail.com>
2023-03-13 04:32:42 -0700

    Oh na na, what's my name?

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <studio.h>
+
+/**
+ * main - prints the name of the program
+ * @argc: counts the number of arguements
+ * @argv: contains an array of arguments
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(int argc, char **argv)
+{
+	(void)argc;
+
+	printf("%s\n", argv[0]);
+	return (0);
+}
